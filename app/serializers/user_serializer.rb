b9ed73b5d27eea88b6d13b620a09f7ab10a5bb48
Angela <angelalamluu@gmail.com>
2021-08-17 17:07:07 -0700

    edited serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
     attributes :id, :username, :bio, :avatar
-    has_many :questions
-    has_many :answers
-    has_many :votes
+    # has_many associations
 end
