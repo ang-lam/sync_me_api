27f33c428539e5d28d4f8e2c061eeac9bffed3f0
Angela <angelalamluu@gmail.com>
2021-08-21 00:47:37 -0700

    fixed create user error in render json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     if user.valid?
         payload = {user_id: user.id}
         token = encode_token(payload)
-        render json: {user: UserSerializer.new(user), jwt: token}, status: :created
+        render json: {user: user, jwt: token}
     else
         render json: {errors: user.errors.full_messages}, status: :not_acceptable
     end
