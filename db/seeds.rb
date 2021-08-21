d31af3d4ebb0153d705e9fefd7efcbb65a3668ef
Angela <angelalamluu@gmail.com>
2021-08-21 00:31:15 -0700

    created seed file for app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+angela = User.create(firstName: 'Angela', lastName: 'Lam', company: 'Microsoft', bio: 'New to the tech field! Would love to connect with SWEs with more experience!', role: 'mentee', email: 'angela@email.com', password: '123456')
+
+User.create(firstName: 'Bob', lastName: 'Smit', company: 'Amazon', bio: 'Five years as a SWE. Happy to share my knowledge!', role: 'mentor', email: 'bob@email.com', password: '123456')
+
+User.create(firstName: 'Thomas', lastName: 'Williams', company: 'Yahoo', bio: 'I want to meet and help those new to the field.', role: 'mentor', email: 'thomas@email.com', password: '123456')
+
+User.create(firstName: 'Emily', lastName: 'Lee', company: 'Facebook', bio: 'Two years in the field but I want to learn more!', role: 'mentee', email: 'emily@email.com', password: '123456')
+
+anna = User.create(firstName: 'Anna', lastName: 'Kim', company: 'Google', bio: 'Senior developer at Google. Excited to talk to new faces!', role: 'mentor', email: 'anna@email.com', password: '123456')
+
+Connection.create(mentor_id: anna.id, mentee_id: angela.id)
