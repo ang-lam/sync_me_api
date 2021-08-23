852ac1334afde545ca5f158b71d0dde8e6da851a
Angela <angelalamluu@gmail.com>
2021-08-22 23:46:53 -0700

    updated UserSerializer with fast json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
-class UserSerializer < ActiveModel::Serializer
-    attributes :id, :username, :bio, :avatar
-    # has_many associations
-end
+class UserSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :firstName, :lastName, :bio, :company, :email, :role
+  has_many :mentors
+  has_many :mentees
+end 
