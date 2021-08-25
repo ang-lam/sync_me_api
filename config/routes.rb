8592d535d8a301aba8304504d9ca3b4046dfcdf6
Angela <angelalamluu@gmail.com>
2021-08-25 15:28:13 -0700

    created a post model in db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   # resources :connections
   # resources :users
   resources :users, only: [:create, :index, :show] #handle signup, query users
