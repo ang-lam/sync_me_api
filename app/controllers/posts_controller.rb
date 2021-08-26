982b03477500e9fd1aff9fa19d5a553ea123481e
Angela <angelalamluu@gmail.com>
2021-08-25 18:17:09 -0700

    added actions to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :update, :destroy]
+  skip_before_action :require_login, only: [:index, :create, :destroy]
+
+  # GET /posts
+  def index
+    posts = Post.all
+ 
+    render json: posts
+  end
+
+  # POST /posts
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post
+      # render json: connection, status: :created, location: connection
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    render json: {message: 'You have successfully deleted your post!'}
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:user_id, :content)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 angela = User.create(firstName: 'Angela', lastName: 'Lam', company: 'Microsoft', bio: 'New to the tech field! Would love to connect with SWEs with more experience!', role: 'mentee', email: 'angela@email.com', password: '123456')
 
-User.create(firstName: 'Bob', lastName: 'Smit', company: 'Amazon', bio: 'Five years as a SWE. Happy to share my knowledge!', role: 'mentor', email: 'bob@email.com', password: '123456')
+bob = User.create(firstName: 'Bob', lastName: 'Smit', company: 'Amazon', bio: 'Five years as a SWE. Happy to share my knowledge!', role: 'mentor', email: 'bob@email.com', password: '123456')
 
 User.create(firstName: 'Thomas', lastName: 'Williams', company: 'Yahoo', bio: 'I want to meet and help those new to the field.', role: 'mentor', email: 'thomas@email.com', password: '123456')
 
@@ -18,3 +18,9 @@ User.create(firstName: 'Emily', lastName: 'Lee', company: 'Facebook', bio: 'Two 
 anna = User.create(firstName: 'Anna', lastName: 'Kim', company: 'Google', bio: 'Senior developer at Google. Excited to talk to new faces!', role: 'mentor', email: 'anna@email.com', password: '123456')
 
 Connection.create(follower_id: anna.id, followed_id: angela.id)
+
+Connection.create(follower_id: angela.id, followed_id: bob.id)
+
+Post.create(user_id: bob.id, content: "First post!")
+
+Post.create(user_id: anna.id, content: "Is this how you make a post?")
