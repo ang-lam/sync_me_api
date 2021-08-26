a76a785e48a56d1acc5c308a6026245bfb8b17d4
Angela <angelalamluu@gmail.com>
2021-08-25 17:19:53 -0700

    added associations to user and post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
     has_many :following_user, foreign_key: :followed_id, class_name: "Connection"
     has_many :followers, through: :following_user
 
+    has_many :posts
+
     has_secure_password
     validates :email, uniqueness: {case_sensitive: false}
     validates :password, presence: true, length: {minimum: 5}, allow_nil: true
