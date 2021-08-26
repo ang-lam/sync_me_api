00b7d926ec70fbd4e5a4e725bd031ddd168fad42
Angela <angelalamluu@gmail.com>
2021-08-25 17:33:18 -0700

    added posts routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # resources :users
   resources :users, only: [:create, :index, :show] #handle signup, query users
   resources :connections, only: [:index, :create]
+  resources :posts, only: [:index, :create, :destroy]
   post '/login', to: "auth#login" #handles login for exisiting users
   get '/auto_login', to: "auth#auto_login" #handles auto login when user revisits
   delete '/delete_connection', to: "connections#delete"
