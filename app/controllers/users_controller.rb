4f5cf8afa543f8cfbe6e430fc8969553a6368526
Angela <angelalamluu@gmail.com>
2021-09-01 23:22:59 -0700

    removed unused actions from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :require_login
-  before_action :set_user, only: [:show, :update, :destroy]
   
   # GET /users
   def index
     users = User.all
 
     render json: users, include: [:followers, :followed, :posts]
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
   end
 
   # POST /users
@@ -26,26 +20,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :firstName, :lastName, :company, :bio, :email, :password_digest, :picture, :password)
