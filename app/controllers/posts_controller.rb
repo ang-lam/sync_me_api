07680ab375095d569f3092f5606d5d28a625c172
Angela <angelalamluu@gmail.com>
2021-09-01 23:21:47 -0700

    removed unused actions from posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :update, :destroy]
   skip_before_action :require_login, only: [:index, :create, :destroy]
 
   # GET /posts
@@ -21,18 +20,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/:id
-  def destroy
-    @post.destroy
-    render json: {message: 'You have successfully deleted your post!'}
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:user_id, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :index]
   resources :connections, only: [:index, :create]
   resources :messages, only: [:index, :create]
-  resources :posts, only: [:index, :create, :destroy]
+  resources :posts, only: [:index, :create]
   post '/login', to: "auth#login" #handles login for exisiting users
   get '/auto_login', to: "auth#auto_login" #handles auto login when user revisits
   delete '/delete_connection', to: "connections#delete"
