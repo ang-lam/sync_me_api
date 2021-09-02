2bf936a8124f196164c3c06eee902f7f214fb68b
Angela <angelalamluu@gmail.com>
2021-09-01 23:20:54 -0700

    removed unused actions in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:show, :update, :destroy]
   skip_before_action :require_login, only: [:index, :create, :delete]
 
   # GET /messages
@@ -7,11 +6,6 @@ class MessagesController < ApplicationController
     messages = Message.all
 
     render json: messages, include: [:sender, :recipient]
-  end
-
-  # GET /messages/1
-  def show
-    render json: @message
   end
 
   # POST /messages
@@ -25,26 +19,7 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1
-  def update
-    if @message.update(message_params)
-      render json: @message
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /messages/1
-  def destroy
-    @message.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def message_params
       params.require(:message).permit(:sender_id, :recipient_id, :content)
