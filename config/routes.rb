bc707392ee204f19abf750a662075dbcf08a0f1d
Angela <angelalamluu@gmail.com>
2021-09-01 23:20:03 -0700

    updated routes.rb with necessary actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :messages
-  # resources :connections
-  # resources :users
-  resources :users, only: [:create, :index, :show] #handle signup, query users
+
+  resources :users, only: [:create, :index]
   resources :connections, only: [:index, :create]
+  resources :messages, only: [:index, :create]
   resources :posts, only: [:index, :create, :destroy]
   post '/login', to: "auth#login" #handles login for exisiting users
   get '/auto_login', to: "auth#auto_login" #handles auto login when user revisits
